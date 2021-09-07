426577c7e6a31f946cb41f27d706342fde46f4df
Ramon Sorage <rsorage@gmail.com>
2021-09-07 14:44:42 -0300

    Create blog on server

--- blog/server/server.go
+++ blog/server/server.go
@@ -1,22 +1,71 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/rsorage/grpc-go-course/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+var collection *mongo.Collection
+
 type server struct{}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	blog := req.GetBlog()
+
+	data := blogItem{
+		AuthorID: blog.GetAuthorId(),
+		Content:  blog.GetContent(),
+		Title:    blog.GetTitle(),
+	}
+
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Cannot convert to OID: %v", err))
+	}
+
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: blog.GetAuthorId(),
+			Content:  blog.GetContent(),
+			Title:    blog.Title,
+		},
+	}, nil
+}
 
 func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	log.Println("Blog Service Started!")
+
+	client := connectMongo()
+	collection = client.Database("grpc_blogs").Collection("blogs")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -44,5 +93,22 @@ func main() {
 	s.Stop()
 	log.Println("Closing the listener...")
 	lis.Close()
+	log.Println("Closing MongoDB connection...")
+	client.Disconnect(context.TODO())
 	log.Println("Bye!")
+}
+
+func connectMongo() *mongo.Client {
+	uri := "mongodb://localhost:27017"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Println("Connecting to MongoDB server...")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("Erro connecting to MongoDB server: %v", err)
+	}
+
+	log.Println("Connected successfully to MongoDB server!")
+	return client
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/rsorage/grpc-go-course
 go 1.16
 
 require (
+	go.mongodb.org/mongo-driver v1.7.2
+	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
